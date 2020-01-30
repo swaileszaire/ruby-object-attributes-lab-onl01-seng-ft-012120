6bb6c306965f24e65020a06094a187819aa1311e
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-30 04:18:20 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+  
+  def initialize
+    @name = @name 
+  end
+  
+  def name 
+    @name 
+  end
+  
+  @name = Dog.new
+  name = "Leetow"
+end 
+    
+  Class Breed 
+  
+  def initialize
+    @breed = @breed
+  end 
+  
+  def breed
+    @breed 
+  end
+  
+  @breed = Breed.new 
+  breed = "Shina Inu" 
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person 
+
+  def initialize
+    @name = @name
+  end
+  
+  @name = Person.new 
+  name = "David"
+end
+
+  def initialize
+  @job = @job
+end
+    
+  @job = Job.new
+  job = "Home Depot"
+end
