a6a3a38567e0d7fa72f7996688540a791ebfd5fb
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-30 04:09:14 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+  
+  def initialize(name)
+    @name = @name 
+  end
+  
+  def 
+    @name 
+  end
+  
+  name = "Leetow"
+  @name = Dog.new
+end 
+
+
+class Breed 
+    
+  def initialize(breed)
+    @breed = @breed
+  end 
+  
+  def breed
+    @breed 
+  end
+  
+  @breed = Breed.new 
+  breed = "Shina Inu" 
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person 
+
+  def initialize(name)
+    @name = @name
+  end
+  
+  name = "David"
+  @name = Person.new 
+end
+
+
+class Job 
+
+  def initialize(job)
+  @job = @job
+    
+  job = "Home Depot"
+  @job = Job.new
+end
